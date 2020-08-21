054a9762d031b26a415756de7e8f890e11cba3cd
ibnu-ja <ibnu.magang@gmail.com>
2020-08-21 17:20:14 +0700

    create tabel permohonan

--- database/migrations/2020_08_10_025932_create_permohonans_table.php
+++ database/migrations/2020_08_10_025932_create_permohonans_table.php
@@ -15,12 +15,13 @@ class CreatePermohonansTable extends Migration
     {
         Schema::create('permohonans', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->string('status');
-            $table->string('no_surat_masuk');
-            $table->string('tanda_terima');
-            $table->string('keterangan');
-            $table->unsignedBigInteger('pelayanans_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('no_surat_masuk')->nullable();
+            $table->string('tanda_terima')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->string('pemohon')->nullable();
+            $table->unsignedBigInteger('pelayanans_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('pelayanans_id')->references('id')->on('layanans');
             $table->timestamps();
