08ac71fc977464b45227257d17fed3ef77c049c2
ibnu-ja <ibnu.magang@gmail.com>
2020-08-21 17:21:30 +0700

    jenis pelayanan base list

--- resources/js/pages/Web/sections/JenisPelayanan.vue
+++ resources/js/pages/Web/sections/JenisPelayanan.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-theme-provider dark>
+  <v-theme-provider>
     <base-section
       id="jenis-pelayanan"
       space="36"
@@ -9,12 +9,94 @@
         class="ma-0"
         justify="center"
       >
-      <v-col cols=12 md=4>
+        <v-col
+          cols=12
+          md=4
+        >
+          <v-card
+            class="mx-auto"
+            width="600"
+          >
+            <v-list>
+              <v-list-group
+                v-for="(item, i) in items"
+                :key="i"
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title>{{item.nama}}</v-list-item-title>
+                  </v-list-item-content>
+                </template>
+                <v-list-group
+                  no-action
+                  sub-group
+                  v-for="(itemm, j) in item.subkategori"
+                  :key="j"
+                >
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title>{{itemm.nama}}</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <v-list-item
+                    v-for="(itemmm, k) in itemm.subkategori"
+                    :key="k"
+                    link
+                  >
+                    <v-list-item-title v-text="itemmm.nama"></v-list-item-title>
+                  </v-list-item>
+                </v-list-group>
+                <!-- <v-list-item
+                  v-for="(itemm, j) in item.subkategori"
+                  :key="j"
+                  link
+                >
+                  <v-list-item-title v-text="itemm.nama"></v-list-item-title>
+                </v-list-item> -->
+              </v-list-group>
+              <!-- <v-list-item v-for="(item, i) in items" :key="i">
+                <v-list-item-title>{{item.nama}}</v-list-item-title>
+              </v-list-item> -->
+              <!-- <v-list-item v-for="(itemm,j) in item.subkategori" :key="j">
+                <v-list-item-title>{{itemm.nama}}</v-list-item-title>
+              </v-list-item> -->
+              <!-- <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
 
-      </v-col>
-      <v-col cols=12 md=8>
+                <v-list-item-title>Pelayanan</v-list-item-title>
+              </v-list-item>
+              <v-list-group
+                no-action
+                sub-group
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title>Admin</v-list-item-title>
+                  </v-list-item-content>
+                </template>
 
-      </v-col>
+                <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  link
+                >
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                  <v-list-item-icon>
+                    <v-icon v-text="item.icon"></v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-group> -->
+            </v-list>
+          </v-card>
+        </v-col>
+        <v-col
+          cols=12
+          md=8
+        >
+
+        </v-col>
         <!-- <base-title
           space="0"
           title="GET OUR NEWSLETTER"
@@ -47,7 +129,80 @@
 </template>
 
 <script>
-  export default {
-    name: 'JenisPelayanan',
-  }
+export default {
+  name: 'JenisPelayanan',
+  data: () => ({
+    items: [
+      {
+        nama: "Pelayanan Bidang Tata Kelola",
+        subkategori: [
+          {
+            nama: "Pelayana Barang Publik",
+            subkategori: [
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              },
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              }
+            ]
+          },
+          {
+            nama: "Pelayana Barang Publik",
+            subkategori: [
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              },
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        nama: "Pelayanan Bidang Tata Kelola",
+        subkategori: [
+          {
+            nama: "Pelayana Barang Publik",
+            subkategori: [
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              },
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              }
+            ]
+          },
+          {
+            nama: "Pelayana Barang Publik",
+            subkategori: [
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              },
+              {
+                nama: "Pengadaan Barang dan Jasa melalui UKPBJ",
+                link: "/asd"
+              }
+            ]
+          }
+        ]
+      }
+    ],
+
+    // cruds: {
+    //   {text:'Create', icon:'mdi-add'},
+    //   {text:'Read',  icon:'mdi-insert_drive_file'},
+    //   {text:'Update',  icon:'mdi-update'},
+    //   {text:'Delete',  icon:'mdi-delete'}
+    // }
+  }),
+}
 </script>
