c6072158137521ff20f3bcd6b97ab956adf16552
ibnu-ja <ibnu.magang@gmail.com>
2020-08-24 13:33:02 +0700

    layanan relation

--- app/Layanan.php
+++ app/Layanan.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Layanan extends Model
 {
-    //
+    
+    public function permohonan()
+    {
+        return $this->hasMany('App\Permohonan');
+    }
 }
