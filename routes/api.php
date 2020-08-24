c4ae00f34748954cac7758d9dac47143d9df709c
ibnu-ja <ibnu.magang@gmail.com>
2020-08-24 13:32:26 +0700

    Routing testing utk store dan tampil permohonan

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,12 +27,28 @@ Route::prefix('auth')->group(function () {
 
 Route::group(['middleware' => 'auth:api'], function () {
     // Users
-    Route::get('users', 'UserController@index')->middleware('isAdmin');
-    Route::get('users/{id}', 'UserController@show')->middleware('isAdminOrSelf');
+    // Route::get('users', 'UserController@index')->middleware('isAdmin');
+    // Route::get('users/{id}', 'UserController@show')->middleware('isAdminOrSelf');
+
+    Route::get('testing', function () {
+        
+        auth()->user()->syncRoles(['operator']);
+        // auth()->user()->syncRoles(['operator', 'admin']);
+        // return auth()->user()->can(['add permohonan','view permohonan']);
+        // $role = auth()->user()->getRoleNames();
+        // return auth()->user()->can('add permohonan');
+        // return response()->json(['roles' => $role]);
+        // return auth()->user()->hasAnyRole(['operator']);
+        // return response()->json(['roles' => $role]);
+    });
+
+    Route::group(['middleware' => ['permission:add permohonan|view permohonan']], function () {
+        Route::get('permohonan', 'PermohonanController@index');
+        Route::post('permohonan/{id}', 'PermohonanController@store');
+    });
 });
 
 Route::group(['middleware' => 'auth:api', 'prefix' => 'admin'], function () {
-
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
