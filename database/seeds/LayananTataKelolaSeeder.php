6485deba551ca7410663936ee0c4fadf065c9c05
ibnu-ja <ibnu.magang@gmail.com>
2020-08-24 13:36:32 +0700

    tambah seeder deskripsi layanan

--- database/seeds/LayananTataKelolaSeeder.php
+++ database/seeds/LayananTataKelolaSeeder.php
@@ -18,7 +18,16 @@ class LayananTataKelolaSeeder extends Seeder
             'kategori' => 'Pelayanan Bidang Tata Kelola',
             'tingkat_layanan' => 'Pusat,Kanwil',
             'subkategori' => '1',
-            'deskripsi' => 'Pelayanan pendaftaran pengadaan barang dan jasa melalui Unit Kerja Pengadaan Barang dan Jasa',
+            'deskripsi' => "<p>Deskripsi: Pelayanan&nbsp;pendaftaran&nbsp;pengadaan&nbsp;barang&nbsp;dan&nbsp;jasa&nbsp;melalui&nbsp;Unit&nbsp;Kerja&nbsp;Pengadaan&nbsp;Barang&nbsp;dan&nbsp;Jasa</p>
+            <p>Syarat:</p>
+            <ol>
+              <li>Mengajukan surat permohonan dari instansi, organiasi dan Lembaga</li>
+              <li>Sistem mekanisme dan prosedur kunsultasi</li>
+              <li>Waktu dan tempat kunsultasi</li>
+              <li>Judul permasalahan Kunsultasi</li>
+              <li>Maksud Tujuan kunsultasi</li>
+              <li>Jumlah yang mengikuti audensi kunsultasi</li>
+            </ol>",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
@@ -27,7 +36,24 @@ class LayananTataKelolaSeeder extends Seeder
             'kategori' => 'Pelayanan Bidang Tata Kelola',
             'tingkat_layanan' => 'Pusat,Kanwil',
             'subkategori' => '1',
-            'deskripsi' => 'Pelayanan penyaluran bantuan operasional kepada Forum Kerukunan Umat Beragama (FKUB)',
+            'deskripsi' => "<p>Deskripsi: Pelayanan&nbsp;penyaluran&nbsp;bantuan&nbsp;operasional&nbsp;kepada&nbsp;Forum&nbsp;Kerukunan&nbsp;Umat&nbsp;Beragama&nbsp;(FKUB)</p>
+            <p>Syarat:</p>
+            <ol>
+              <li>FKUB mengajukan proposal yang memuat :
+                <ul>
+                  <li>Jumlah / nilai uang bantuan</li>
+                  <li>Nomor Rekening a.n FKUB</li>
+                </ul>
+              </li>
+              <li>SK Penetapan Bantuan
+                <ul>
+                  <li>Perjanjian Kerjasama (PK)</li>
+                  <li>Rancangan BIaya Anggaran</li>
+                  <li>Surat Pernyataan Pertang Jawaban Mutlah (SPTJM)</li>
+                  <li>Kwitansi Penerimaan Bantuan</li>
+                </ul>
+              </li>
+            </ol>",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
