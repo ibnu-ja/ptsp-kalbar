ccf273bdf086a480c2cf46d346a1b0bd8a01f69a
ibnu-ja <ibnu.magang@gmail.com>
2020-08-25 23:53:29 +0700

    Kembali masukkan dashboard ke dalam komponen appbar (karena dalam appbar ada tombol drawer)

--- resources/js/layouts/admin/AppBar.vue
+++ resources/js/layouts/admin/AppBar.vue
@@ -1,11 +1,14 @@
 <template>
+    <div>
+      <dash-drawer v-model="drawer" />
+
     <v-app-bar
       app
       color="indigo"
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         text
@@ -16,10 +19,19 @@
         <v-icon right>mdi-account-arrow-right</v-icon>
       </v-btn>
     </v-app-bar>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'DashbAppBar',
+  components: {    
+      DashDrawer: () => import('@/layouts/admin/Drawer'),
+  },
+  data() {
+    return {
+      drawer: null
+    }
+  }
 } 
 </script>
