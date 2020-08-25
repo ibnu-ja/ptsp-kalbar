b56ebb84b0fed1b5b372840b933098423749e722
ibnu-ja <ibnu.magang@gmail.com>
2020-08-25 23:03:17 +0700

    perbarui drawer untuk dashboard agar bisa menampilkan submenu

--- resources/js/layouts/admin/Drawer.vue
+++ resources/js/layouts/admin/Drawer.vue
@@ -1,44 +1,74 @@
 <template>
-  <!-- <v-navigation-drawer
-    fixed
-    temporary
-    v-bind="$attrs"
-    v-on="$listeners"
-  >
-    <v-list
-      color="white"
-    >
-      <v-list-item
-        v-for="(name, i) in items"
-        :key="i"
-        :to="name.link"
-        :exact="name === 'Home'"
-        color="primary"
-      >
-        <v-list-item-content>
-          <v-list-item-title v-text="name.title" />
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer> -->
   <v-navigation-drawer
     v-model="drawer"
     app
   >
     <v-list dense>
-      <v-list-item
-        v-for="(item, i) in items"
-        :key="i"
-        :to="item.link"
-        exact
-      >
-        <v-list-item-action>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>{{item.title}}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <template v-for="item in items">
+        <v-row
+          v-if="item.heading"
+          :key="item.heading"
+          align="center"
+        >
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-col>
+          <v-col
+            cols="6"
+            class="text-center"
+          >
+            <a
+              href="#!"
+              class="body-2 black--text"
+            >EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(child, i) in item.children"
+            :key="i"
+            link
+          >
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ child.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item
+          v-else
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -46,16 +76,43 @@
 <script>
 export default {
   name: 'DashDrawer',
-  data() {
-    return {
-      drawer: null
-    }
-  },
-  props: {
-    items: {
-      type: Array,
-      default: () => ([]),
-    },
-  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: 'mdi-home', text: 'PTSP' },
+      { icon: 'mdi-view-dashboard', text: 'Dashboard' },
+      // { icon: 'mdi-handshake', text: 'Layanan' },
+      {
+        icon: 'mdi-chevron-up',
+        'icon-alt': 'mdi-chevron-down',
+        text: 'Layanan',
+        model: true,
+        children: [
+          { icon: 'mdi-handshake', text: 'List' },
+          { icon: 'mdi-hand-pointing-right', text: 'Disposisi' },
+          // { icon: 'mdi-plus', text: 'Create label' },
+        ],
+      },
+      // {
+      //   icon: 'mdi-chevron-up',
+      //   'icon-alt': 'mdi-chevron-down',
+      //   text: 'Layanan',
+      //   model: false,
+      //   children: [
+      //     { text: 'Import' },
+      //     { text: 'Export' },
+      //     { text: 'Print' },
+      //     { text: 'Undo changes' },
+      //     { text: 'Other contacts' },
+      //   ],
+      // },
+      // { icon: 'mdi-cog', text: 'Settings' },
+      // { icon: 'mdi-message', text: 'Send feedback' },
+      // { icon: 'mdi-help-circle', text: 'Help' },
+      // { icon: 'mdi-cellphone-link', text: 'App downloads' },
+      // { icon: 'mdi-keyboard', text: 'Go to the old version' },
+    ],
+  }),
 }
 </script>
