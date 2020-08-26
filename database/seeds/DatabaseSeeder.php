7fbd3a76a5ecccdd3f75f1ad582f3119f1ff1878
ibnu-ja <ibnu.magang@gmail.com>
2020-08-26 15:41:14 +0700

    disable seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(LayananTataKelolaSeeder::class);
-        $this->call(LayananAgamaSeeder::class);
-        $this->call(LayananPendidikanSeeder::class);
-        $this->call(LayananHajiUmrahSeeder::class);
-        $this->call(LayananSertifikasiHalal::class);
+        // $this->call(LayananTataKelolaSeeder::class);
+        // $this->call(LayananAgamaSeeder::class);
+        // $this->call(LayananPendidikanSeeder::class);
+        // $this->call(LayananHajiUmrahSeeder::class);
+        // $this->call(LayananSertifikasiHalal::class);
         // buat role
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'pimpinan']);
@@ -26,20 +26,34 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'petugas']);
         Role::create(['name' => 'operator']);
         Role::create(['name' => 'user']);
+        //permohonan
         Permission::create(['name' => 'view permohonan']);
         Permission::create(['name' => 'add permohonan']);
         Permission::create(['name' => 'edit permohonan']);
         Permission::create(['name' => 'delete permohonan']);
         Permission::create(['name' => 'edit status permohonan']);
+        //layanan
+        Permission::create(['name' => 'view layanan']);
+        Permission::create(['name' => 'add layanan']);
+        Permission::create(['name' => 'edit layanan']);
+        Permission::create(['name' => 'delete layanan']);
+        //disposisi
         Permission::create(['name' => 'disposisi']);
 
         // tambahkan permission ke role admin
         $role = Role::find(1);
-        $role->givePermissionTo('disposisi');        
+        $role->givePermissionTo('disposisi');
+
         $role->givePermissionTo('view permohonan');
         $role->givePermissionTo('add permohonan');
         $role->givePermissionTo('edit permohonan');
         $role->givePermissionTo('delete permohonan');
+
+        $role->givePermissionTo('view layanan');
+        $role->givePermissionTo('add layanan');
+        $role->givePermissionTo('edit layanan');
+        $role->givePermissionTo('delete layanan');
+
         $role->givePermissionTo('edit status permohonan');
 
         // tambah permission ke role pimpinan
