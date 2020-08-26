9022010de4e74e65036766f46431f0f34b99ce77
ibnu-ja <ibnu.magang@gmail.com>
2020-08-26 21:20:06 +0700

    Return empty response jika layanan blum ada data

--- app/Http/Controllers/TampilLayananController.php
+++ app/Http/Controllers/TampilLayananController.php
@@ -2,32 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\LayananResource;
 use Illuminate\Http\Request;
 use App\Layanan;
 
 class TampilLayananController extends Controller
 {
-    // public function index($layanan)
-    // {
-    //     $layanans = Layanan::where('kategori', 'like', $layanan)->get();
-    //     return response()->json(['data' => $layanans], 200);
-    // }
-
     public function index(Request $request)
     {
         $layanans = Layanan::where([
             ['kategori', 'like', $request->kategori],
             ['subkategori', '=', $request->subkategori]
         ])->get();
-        return response()->json(['data' => $layanans], 200);
+        return new LayananResource($layanans);
     }
 
     public function kategori()
     {
         $kategoris = Layanan::select('kategori')->groupBy('kategori')->get();
-        $kategori = array();
+        // $kategori = array();
+
         foreach ($kategoris as $item) {
             $layanan[] = $item->kategori;
+        }
+        if (empty($layanan)) {
+            return response()->json(null, 204);
         }
         return response()->json(['kategori' => $layanan], 200);
     }
