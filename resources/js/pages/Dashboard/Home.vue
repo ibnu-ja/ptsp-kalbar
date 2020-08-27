4d6fa3d48e15e683e9fe4c3515ca20095f81f5b1
ibnu-ja <ibnu.magang@gmail.com>
2020-08-27 13:34:58 +0700

    testing permission

--- resources/js/pages/Dashboard/Home.vue
+++ resources/js/pages/Dashboard/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="component-wrap">
     <div class="">
+      {{$auth.check({permissions: ['add layanan']})}}
+      <!-- {{this.$auth.user().permission}} -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Beranda',
   data () {
     return {
     }
+  },
+  methods: {    
+  },
+  mounted () {
+    // getUser()
+    // console.log('asdasd')
   }
 }
 </script>
