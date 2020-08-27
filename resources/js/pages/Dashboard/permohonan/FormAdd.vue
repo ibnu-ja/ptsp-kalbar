0bec9c7740b59ba977ee279ce7c7af45faaeac8c
ibnu-ja <ibnu.magang@gmail.com>
2020-08-28 00:41:10 +0700

    Form Tambah Permohonan

--- resources/js/pages/Dashboard/permohonan/FormAdd.vue
+++ resources/js/pages/Dashboard/permohonan/FormAdd.vue
@@ -12,106 +12,250 @@
         </ul>
       </v-alert>
     </div>
-    <v-card>
-      <v-card-title>
-        Tambah Layanan
-      </v-card-title>
-      <v-divider></v-divider>
-      <ValidationObserver
-        v-slot="{ validate, reset, invalid }"
-        ref="observer"
+    <v-row>
+      <v-col
+        cols="12"
+        md="4"
       >
-        <v-form
-          lazy-validation
-          @submit.prevent="submit"
-        >
+        <v-card>
+          <v-card-title>
+            Pilih Layanan
+          </v-card-title>
+          <v-divider>
+
+          </v-divider>
           <v-card-text>
             <v-row
               no-gutters
-              align="center"
               justify="center"
             >
-              <v-col cols=12>
-                <ValidationProvider
-                  v-slot="{ errors }"
-                  name="Nama layanan"
-                  rules="required"
-                >
-                  <v-text-field
-                    label="Nama"
-                    :error-messages="errors"
-                    v-model="items.name"
-                  ></v-text-field>
-                </ValidationProvider>
-              </v-col>
-              <v-col cols=12>
-                <ValidationProvider
-                  v-slot="{ errors }"
-                  name="Kategori layanan"
-                  rules="required"
-                >
-                  <v-combobox
-                    v-model="items.kategori"
-                    :error-messages="errors"
-                    :items="kategori"
-                    label="Kategori layanan"
-                  ></v-combobox>
-                </ValidationProvider>
-              </v-col>
-              <v-col cols=12>
-                <ValidationProvider
-                  v-slot="{ errors }"
-                  name="Subkategori layanan"
-                  rules="required"
-                >
-                  <v-select
-                    :items="subkategori"
-                    v-model="items.subkategori"
-                    :error-messages="errors"
-                    item-text="text"
-                    item-value="value"
-                    label="Subkategori"
-                  ></v-select>
-                </ValidationProvider>
-              </v-col>
               <v-col cols="12">
-                <div>Deskripsi</div>
-                <ckeditor
-                  :editor="editor"
-                  v-model="items.deskripsi"
-                  :config="editorConfig"
-                ></ckeditor>
+                <v-autocomplete
+                  v-model="kategori"
+                  :items="kategoris"
+                  label="Bidang Layanan"
+                ></v-autocomplete>
+              </v-col>
+              <v-col
+                cols="12"
+                v-if="kategori"
+              >
+                <v-autocomplete
+                  v-model="layanan"
+                  :items="layanans"
+                  item-text="name"
+                  item-value="id"
+                  label="Layanan"
+                ></v-autocomplete>
               </v-col>
             </v-row>
           </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="secondary"
-              @click="clear"
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="8"
+      >
+        <v-card>
+          <v-card-title>
+            Tambah
+          </v-card-title>
+          <v-divider></v-divider>
+          <ValidationObserver
+            v-slot="{ validate, reset, invalid }"
+            ref="observer"
+          >
+            <v-form
+              lazy-validation
+              :disabled="!layanan"
+              @submit.prevent="submit"
             >
-              clear
-            </v-btn>
-            <v-btn
-              type="submit"
-              class="mr-4"
-              color="primary"
-              :disabled="invalid"
-            >
-              submit
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </ValidationObserver>
-    </v-card>
+              <v-card-text>
+                <v-row
+                  no-gutters
+                  justify="center"
+                >
+                  <v-col cols=12>
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="Pemohon"
+                      rules="required"
+                    >
+                      <v-text-field
+                        label="Pemohon"
+                        :error-messages="errors"
+                        v-model="items.pemohon"
+                        clearable
+                      ></v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="Keterangan"
+                      rules="required"
+                    >
+                      <v-text-field
+                        label="Keterangan"
+                        :error-messages="errors"
+                        v-model="items.keterangan"
+                        clearable
+                      ></v-text-field>
+                    </ValidationProvider>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-btn
+                      text
+                      :disabled="!layanan"
+                      @click="tambahBerkas()"
+                    >
+                      <v-icon>mdi-plus</v-icon>Tambah dokumen
+                    </v-btn>
+                  </v-col>
+                  <v-col
+                    cols=12
+                    v-if="!(items.asal.length === 0)"
+                  >
+                    <template v-for="(berkas, i) in items.asal">
+                      <v-divider></v-divider>
+                      <v-row>
+                        <v-col>
+                          <span
+                            class="text-caption"
+                            v-bind:class="(!layanan)?'text--disabled':''"
+                          >Berkas {{i+1}}</span>
+                        </v-col>
+                        <v-col class="text-right">
+                          <v-btn
+                            icon
+                            small
+                            :disabled="!layanan"
+                            @click="hapusBerkas(i)"
+                          >
+                            <v-icon>mdi-close</v-icon>
+                          </v-btn>
+                        </v-col>
+
+                      </v-row>
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        name="Asal dokumen"
+                        rules="required"
+                      >
+                        <v-text-field
+                          label="Asal dokumen"
+                          :error-messages="errors"
+                          v-model="items.asal[i]"
+                          clearable
+                        ></v-text-field>
+                      </ValidationProvider>
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        name="Nomor dokumen"
+                        rules="required"
+                      >
+                        <v-text-field
+                          label="Nomor dokumen"
+                          :error-messages="errors"
+                          v-model="items.nomor_berkas[i]"
+                          clearable
+                        ></v-text-field>
+                      </ValidationProvider>
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        name="Isi/perihal dokumen"
+                        rules="required"
+                      >
+                        <v-text-field
+                          label="Isi/perihal dokumen"
+                          :error-messages="errors"
+                          v-model="items.perihal[i]"
+                          clearable
+                        ></v-text-field>
+                      </ValidationProvider>
+                      <v-menu
+                        ref="menu"
+                        v-model="menu[i]"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <ValidationProvider
+                            v-slot="{ errors }"
+                            name="Tanggal dokumen"
+                            rules="required"
+                          >
+                            <v-text-field
+                              v-model="items.tgl_format[i]"
+                              label="Tanggal dokumen"
+                              hint="DD/MM/YYYY format"
+                              :error-messages="errors"
+                              persistent-hint
+                              :name="Math.random()"
+                              prepend-icon="mdi-calendar"
+                              v-bind="attrs"
+                              @blur="items.tgl_berkas[i] = parseDate(items.tgl_format[i])"
+                              v-on="on"
+                              clearable
+                            >
+                            </v-text-field>
+                          </ValidationProvider>
+                        </template>
+                        <v-date-picker
+                          v-model="items.tgl_berkas[i]"
+                          no-title
+                          @input="menu[i] = false"
+                        ></v-date-picker>
+                      </v-menu>
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        name="File dokumen"
+                        rules="required"
+                      >
+                        <v-file-input
+                          show-size
+                          :error-messages="errors"
+                          v-model="items.berkas[i]"
+                          label="File berkas"
+                        ></v-file-input>
+                      </ValidationProvider>
+                    </template>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="secondary"
+                  @click="clear"
+                >
+                  clear
+                </v-btn>
+                <v-btn
+                  type="submit"
+                  class="mr-4"
+                  color="primary"
+                  :disabled="invalid"
+                >
+                  submit
+                </v-btn>
+              </v-card-actions>
+            </v-form>
+          </ValidationObserver>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import { format, parseISO } from 'date-fns'
 import { extend, ValidationObserver, ValidationProvider } from "vee-validate";
 import * as rules from "vee-validate/dist/rules";
 import { messages } from "vee-validate/dist/locale/id.json";
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+var FormData = require('form-data');
 
 Object.keys(rules).forEach(rule => {
   extend(rule, {
@@ -126,43 +270,100 @@ export default {
     ValidationObserver
   },
   data () {
-    const self = this;
     return {
-      mal_id: "",
       items: {
-        name: "",
-        kategori: "",
-        subkategori: "",
-        deskripsi: "",
+        pemohon: null,
+        keterangan: null,
+        nomor_berkas: [],
+        berkas: [],
+        asal: [],
+        perihal: [],
+        tgl_berkas: [],
+        tgl_format: [],
       },
-      dialog: true,
       errorMsg: {},
+      menu: [false],
       subkategori: [
         { text: 'Layanan Barang Publik', value: 0 },
         { text: 'Layanan Jasa Publik', value: 1 },
-        { text: 'Layanan Administrasi', value: 3 },
+        { text: 'Layanan Administrasi', value: 2 },
       ],
-      editor: ClassicEditor,
-      kategori: [],
+      kategoris: [],
+      kategori: null,
       alert: false,
-    };
+      layanans: [],
+      layanan: null,
+    }
+  },
+  watch: {
+    kategori: function (val) {
+      var self = this
+      axios.get('/api/tampil-layanan?kategori=' + val)
+        .then(function (response) {
+          // console.log(('/api/tampil-layanan?kategori=' + val))
+          console.log(response.data.data)
+          self.layanans = response.data.data
+        }).catch(function (e) {
+          console.log(e)
+        });
+
+    },
+    'items.tgl_berkas': function (val, oldval) {
+      for (const i in val) {
+        this.items.tgl_format[i] = this.formatDate(this.items.tgl_berkas[i])
+      }
+    },
   },
   mounted () {
     const self = this
+    //get kategori
     axios.get('/api/kategori')
       .then(function (response) {
-        self.kategori = response.data.kategori
+        self.kategoris = response.data.kategori
       }).catch(function (e) {
         console.log(e)
       });
   },
   methods: {
+    formatDate (date) {
+      if (!date) return null
+
+      const [year, month, day] = date.split('-')
+      return `${day}/${month}/${year}`
+    },
+    parseDate (date) {
+      if (!date) return null
+
+      const [month, day, year] = date.split('/')
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    },
+    tambahBerkas () {
+      this.items.asal.push("")
+      this.items.perihal.push("")
+      // this.items.tgl_berkas.push(new Date().toISOString().substr(0, 10)) //tanggal sekarang
+      // this.items.tgl_format.push(this.formatDate(new Date().toISOString().substr(0, 10))) //tanggal sekarang
+      this.items.tgl_berkas.push("")
+      this.items.tgl_format.push("")
+      this.items.nomor_berkas.push("")
+    },
+    hapusBerkas (index) {
+      this.items.asal.splice(index, 1)
+      this.items.perihal.splice(index, 1)
+      this.items.nomor_berkas.splice(index, 1)
+      this.items.berkas.splice(index, 1)
+      this.items.tgl_berkas.splice(index, 1)
+      this.items.tgl_format.splice(index, 1)
+    },
     clear () {
       var self = this
-      self.items.name = ""
-      self.items.kategori = ""
-      self.items.subkategori = ""
-      self.items.deskripsi = ""
+      self.items.pemohon = ""
+      self.items.keterangan = ""
+      self.items.nomor_berkas = []
+      self.items.berkas = []
+      self.items.asal = []
+      self.items.perihal = []
+      self.items.tgl_berkas = []
+      self.items.tgl_format = []
       self.$refs.observer.reset();
     },
     async submit () {
@@ -172,13 +373,23 @@ export default {
       if (!isValid) {
         self.errorMsg = { Validation: "Data tidak valid!" }
       }
-      let payload = {
-        name: "",
-        kategori: self.items.kategori,
-        subkategori: self.items.subkategori,
-        deskripsi: self.items.deskripsi,
+
+      var payload = new FormData();
+      payload.append('pemohon', self.items.pemohon)
+      payload.append('keterangan', self.items.keterangan)
+      for (var i in self.items.asal) {
+        payload.append('asal[]', self.items.asal[i])
+        payload.append('perihal[]', self.items.perihal[i])
+        payload.append('nomor_berkas[]', self.items.nomor_berkas[i])
+        payload.append('tgl_berkas[]', self.items.tgl_berkas[i])
+        payload.append('berkas[]', self.items.berkas[i])
       }
-      axios.post('/api/layanan', payload)
+      console.log(...payload)
+      axios.post('/api/v1/layanan/' + self.layanan + '/permohonan', payload, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
         .then(function (response) {
           self.$store.commit('hideLoader');
           // reset the values ...
@@ -192,7 +403,7 @@ export default {
           // self.dialog = true
 
         });
-    }
+    },
   }
 };
 </script>
