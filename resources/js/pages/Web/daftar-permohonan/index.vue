c4e5867f5c77d7adae54d9dac6ccb57c87048e2f
ibnu-ja <ibnu.magang@gmail.com>
2020-08-28 01:10:14 +0700

    saving commit

---
+++ resources/js/pages/Web/daftar-permohonan/index.vue
@@ -0,0 +1,32 @@
+
+<script>
+// Extensions
+import View from '@/pages/Web/View'
+
+// Mixins
+import LoadSections from '@/mixins/load-sections'
+
+export default {
+  name: 'Home',
+
+  metaInfo: { title: 'Home' },
+
+  extends: View,
+
+  mixins: [
+    LoadSections([
+      'carousel',
+      'proses-pelayanan',
+      'contact-us',
+      'social-media',
+    ]),
+  ],
+
+  props: {
+    id: {
+      type: String,
+      default: 'home',
+    },
+  },
+}
+</script>
---
+++ resources/js/pages/Web/layanan/Permohonan.vue
@@ -0,0 +1,34 @@
+<script>
+  // Extensions
+  // import View from '@/pages/Web/View'
+
+  // // Mixins
+  // import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Layanan',
+
+    metaInfo: { title: 'Daftar Layanan' },
+
+    // extends: View,
+
+    // mixins: [
+    //   LoadSections([
+    //     'hero-alt',
+    //     'jenis-pelayanan',
+    //     // 'theme-features',
+    //     // 'affiliates',
+    //     // 'newsletter',
+    //     // 'pro-features',
+    //     // 'info',
+    //   ]),
+    // ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'about',
+      },
+    },
+  }
+</script>
