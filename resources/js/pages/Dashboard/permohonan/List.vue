3c032e15e8b480607e4e49cf5655826fb4ece2eb
ibnu-ja <ibnu.magang@gmail.com>
2020-08-30 17:11:43 +0700

    PermohonanList

--- resources/js/pages/Dashboard/permohonan/List.vue
+++ resources/js/pages/Dashboard/permohonan/List.vue
@@ -1,77 +1,366 @@
 <template>
   <div class="component-wrap">
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      item-key="name"
-      class="elevation-1"
-      :footer-props="{
+    <v-snackbar
+      v-model="snackbar"
+      timeout="3000"
+      color="success"
+      top
+    >
+      {{ snackbarMsg }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-card>
+      <v-card-title>
+        <v-row>
+          <v-col
+            cols=12
+            md=4
+          >
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Cari"
+              single-line
+              hide-details
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols=12
+            md=4
+          >
+            <v-autocomplete
+              :name="Math.random()"
+              v-model="kategori"
+              :items="kategoris"
+              append-icon=""
+              label="Bidang Layanan"
+              clearable
+            ></v-autocomplete>
+          </v-col>
+          <v-col
+            cols=12
+            md=4
+          >
+            <v-autocomplete
+              :name="Math.random()"
+              v-model="layanan"
+              :items="layanans"
+              item-text="name"
+              item-value="id"
+              append-icon=""
+              label="Layanan"
+              clearable
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-data-table
+        :loading="dataLoading"
+        :headers="headers"
+        :items="items"
+        :search="search"
+        :items-per-page="10"
+        item-key="id"
+        :footer-props="{
       showFirstLastPage: true,
       firstIcon: 'mdi-arrow-collapse-left',
       lastIcon: 'mdi-arrow-collapse-right',
       prevIcon: 'mdi-minus',
       nextIcon: 'mdi-plus'
     }"
-    ></v-data-table>
+      >
+        <template v-slot:body="{ items }">
+          <tbody>
+            <tr
+              v-for="item in items"
+              :key="item.id"
+            >
+              <td>
+                <v-btn
+                  icon
+                  small
+                  color="primary"
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  small
+                  color="red"
+                  @click="konfirmasi(item)"
+                >
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </td>
+              <td>{{item.pemohon}}</td>
+              <td>{{item.layanan.kategori}}</td>
+              <td>{{item.layanan.name}}</td>
+              <td>
+                <v-btn
+                  outlined
+                  rounded
+                  small
+                  color="primary"
+                  @click="tampilBerkas(item.berkas)"
+                >tampil</v-btn>
+              </td>
+              <td>{{item.created_at | moment("DD-MM-YYYY")}}</td>
+              <td>{{item.updated_at | moment("DD-MM-YYYY")}}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-data-table>
+      <!-- <v-card-text v-else><div >Tolon</div></v-card-text> -->
+    </v-card>
+
+    <v-dialog
+      v-model="dialogBerkas"
+      max-width="900"
+    >
+      <v-card>
+        <v-card-title class="headline">Berkas</v-card-title>
+        <v-card-text>
+          <v-data-table
+            :headers="berkasHeaders"
+            :items="berkas"
+            :items-per-page="5"
+          ><template v-slot:body="{ items }">
+              <tbody>
+                <tr
+                  v-for="item in berkas"
+                  :key="item.id"
+                >
+                  <td>
+                    <v-btn
+                      icon
+                      small
+                      color="primary"
+                      @click="unduh(item)"
+                    >
+                      <v-icon>mdi-cloud-download</v-icon>
+                    </v-btn>
+                  </td>
+                  <td>{{item.custom_properties.surat.nomor}}</td>
+                  <td>{{item.custom_properties.surat.asal}}</td>
+                  <td>{{item.custom_properties.surat.perihal}}</td>
+                  <td>{{item.file_name}}</td>
+                  <td>{{item.custom_properties.surat.tgl_berkas}}</td>
+                  <td>{{formatBytes(item.size)}}</td>
+                </tr>
+              </tbody>
+            </template></v-data-table>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialogBerkas = false"
+          >Tutup</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="dialogDelete"
+      persistent
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="headline">Peringatan!</v-card-title>
+        <v-card-text v-html="msg"></v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialogDelete = false"
+          >Batal</v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="deleteData(selectedItem)"
+          >Ya!</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import fileDownload from 'js-file-download'
 export default {
   data () {
     return {
-      headers: [
+      items: [],
+      dataLoading: null,
+      dialogDelete: false,
+      dialogBerkas: false,
+      msg: null,
+      search: '',
+      snackbar: false,
+      snackbarMsg: null,
+      selectedItem: "",
+      deskripsi: null,
+      kategoris: [],
+      layanans: [],
+      kategori: null,
+      layanan: null,
+      berkas: null,
+      berkasHeaders: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Unduh',
+          align: 'center',
+        },
+        { text: 'Nomor', value: 'custom_properties.surat.nomor' },
+        { text: 'Asal', value: 'custom_properties.surat.asal' },
+        { text: 'Perihal', value: 'custom_properties.surat.perihal' },
+        {
+          text: 'Nama file',
           align: 'start',
-          value: 'name',
+          value: 'file_name',
         },
-        { text: 'Category', value: 'category' },
-      ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          category: 'Ice cream',
-        },
-        {
-          name: 'Ice cream sandwich',
-          category: 'Ice cream',
-        },
-        {
-          name: 'Eclair',
-          category: 'Cookie',
-        },
-        {
-          name: 'Cupcake',
-          category: 'Pastry',
-        },
-        {
-          name: 'Gingerbread',
-          category: 'Cookie',
-        },
-        {
-          name: 'Jelly bean',
-          category: 'Candy',
-        },
-        {
-          name: 'Lollipop',
-          category: 'Candy',
-        },
-        {
-          name: 'Honeycomb',
-          category: 'Toffee',
-        },
-        {
-          name: 'Donut',
-          category: 'Pastry',
-        },
-        {
-          name: 'KitKat',
-          category: 'Candy',
-        },
+        { text: 'Tgl berkas', value: 'custom_properties.surat.tgl_berkas' },
+        { text: 'Ukuran', value: 'size' },
       ],
     }
   },
+  watch: {
+    kategori (newVal) {
+      var self = this
+      if (newVal) { //check if not null
+        axios.get('/api/tampil-layanan', {
+          params: {
+            kategori: newVal
+          }
+        })
+          .then(function (response) {
+            self.layanans = response.data.data
+          }).catch(function (e) {
+            console.log(e)
+          });
+      } else {
+        this.tampilData()
+        self.layanans = []
+      }
+    }
+  },
+  created () {
+    this.tampilData()
+    const self = this
+    axios.get('/api/kategori')
+      .then(function (response) {
+        self.kategoris = response.data.kategori
+      }).catch(function (e) {
+        console.log(e)
+      });
+  },
+  computed: {
+    headers () {
+      return [
+        { text: 'Aksi', value: 'name', width: '100', align: 'center' },
+        { text: 'Pemohon', value: 'pemohon' },
+        // { text: 'Kategori', value: 'pemohon' },
+        // { text: 'Pemohon', value: 'pemohon' },
+        {
+          text: 'Kategori', value: 'layanan.kategori',
+          filter: value => {
+            if (!this.kategori) return true
+            return value == this.kategori
+          },
+        },
+        {
+          text: 'Layanan', value: 'layanan.id',
+          filter: value => {
+            if (!this.layanan) return true
+            return value == this.layanan
+          },
+        },
+        { text: 'Berkas', value: 'berkas' },
+        { text: 'Dibuat', value: 'created_at', width: '110' },
+        { text: 'Diperbarui', value: 'created_at', width: '110' },
+      ]
+    },
+  },
+  methods: {
+    formatBytes (bytes, decimals = 2) {
+      if (bytes === 0) return '0 Bytes';
+
+      const k = 1024;
+      const dm = decimals < 0 ? 0 : decimals;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+      const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    },
+    unduh (berkas) {
+      fileDownload('/api/v1/unduh-berkas/' + berkas.id, berkas.file_name)
+    },
+    tampilData () {
+      var self = this;
+      self.dataLoading = true
+      axios.get('/api/v1/layanan/permohonan')
+        .then(function (response) {
+          // console.log(response.data.data)
+          self.items = response.data.data
+          self.dataLoading = false
+        })
+        .catch(function (e) {
+          console.log(e)
+        })
+    },
+    tampilBerkas (berkas) {
+      var self = this
+      self.dialogBerkas = true
+      // console.log(berkas)
+      self.berkas = [...berkas]
+      console.log(self.berkas)
+
+    },
+    konfirmasi (item) {
+      var self = this;
+      self.dialogDelete = true
+      // console.log(key)
+      self.msg = "Anda akan menghapus layanan <span class='font-weight-bold'>" + item.name + "</span>. Tetap lanjutkan?"
+      self.selectedItem = item.id
+    },
+    tampilDeskripsi (item) {
+      var self = this;
+      self.dialogDeskripsi = true
+      console.log(self.dialogDeskripsi);
+      this.deskripsi = item.deskripsi;
+    },
+    deleteData (id) {
+      var self = this;
+      axios.delete('/api/v1/layanan/' + id)
+        .then(function (response) {
+          self.tampilData()
+          self.dialogDelete = false
+          self.snackbarMsg = "Sukses menghapus!"
+          self.snackbar = true;
+        })
+        .catch(function (e) {
+          console.log(e)
+          self.dialogDelete = false
+        })
+    },
+    filterKategori (value, search, item) {
+      return value != null &&
+        search != null &&
+        typeof value === 'string' &&
+        value.toString().toLocaleUpperCase().indexOf(search) !== -1
+    },
+  }
 }
 </script>
