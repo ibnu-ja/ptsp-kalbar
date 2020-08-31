a96a145494db516ace013f8a668269f16e0bb9d4
ibnu-ja <ibnu.magang@gmail.com>
2020-08-31 11:13:50 +0700

    tampil jabatan controller

---
+++ app/Http/Controllers/Api/v1/TampilJabatanController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TampilJabatanController extends ApiController
+{
+    /**
+     * tampilkan pejabat untuk di disposisi
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (Auth::user()->hasRole('pimpinan')) {
+            // return 'pimpinan';
+            $jabatan = User::whereHas("roles", function ($q) {
+                $q->where("name", "pejabat");
+            })->select('jabatan', 'kode_jabatan')->get();
+        } else if (Auth::user()->hasRole('pejabat')) {
+            // return 'pejabat';
+            $jabatan = User::whereHas("roles", function ($q) {
+                $q->where("name", "petugas");
+            })
+                ->where("kode_jabatan", "like", Auth::user()->kode_jabatan . ".%")
+                ->select('jabatan', 'kode_jabatan')->get();
+        }
+        // return 'salah';
+        // $jabatan = User::whereHas("roles", function ($q) {
+        //     $q->where("name", "pejabat");
+        // })->select('jabatan', 'kode_jabatan')->get();
+        // $jabatan = User::whereNotNull('jabatan')->select('jabatan', 'kode_jabatan')->get();
+        return response()->json(['data' => $jabatan]);
+    }
+}
