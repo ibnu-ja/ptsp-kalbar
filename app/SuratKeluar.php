d012607fd9079d42bdff70b0d7b49abeda40c03f
ibnu-ja <ibnu.magang@gmail.com>
2020-09-01 10:25:21 +0700

    Preparing model to use MediaLibrary

--- app/SuratKeluar.php
+++ app/SuratKeluar.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class SuratKeluar extends Model
+class SuratKeluar extends Model implements HasMedia
 {
-    //
+    use InteractsWithMedia;
+    //oh god
+    protected $fillable = [
+        'user_id',
+        'jenis',
+        'isi',
+        'kode_satker',
+        'tgl_surat',
+        'asal',
+        'nomor_urut_biasa',
+        'nomor_urut_spd',
+        'nomor_urut_sk',
+        'nomor_surat'
+    ];
 }
