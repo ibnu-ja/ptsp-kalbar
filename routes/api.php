a217d8ffcab3dfe447f5ad3f7b3385ac04e1a63c
ibnu-ja <ibnu.magang@gmail.com>
2020-09-01 13:09:45 +0700

    api route tampil semua jabatan

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,8 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function () {
         Route::post('orderan/{orderan}/berkas', 'OrderanController@tambahBerkas')->middleware(['permission:delete orderan']);
         Route::get('orderan/{orderan}/selesai', 'OrderanController@selesai')->middleware(['permission:edit status orderan']);
         Route::post('orderan/{orderan}/disposisi', 'OrderanController@disposisi')->middleware(['permission:disposisi']);
-        Route::get('jabatan', 'TampilJabatanController@index')->middleware(['role:pejabat|pimpinan']);
+        Route::get('jabatan', 'TampilJabatanController@index')->middleware(['role:pejabat|pimpinan|admin']);
+        Route::get('jabatan-all', 'TampilJabatanController@indexTwo')->middleware(['role:pegawai|admin']);
 
         Route::group(['middleware' => ['permission:view layanan']], function () {
             Route::get('layanan', 'LayananController@index');
@@ -64,7 +65,18 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function () {
         Route::put('layanan/{layanan}', 'LayananController@update')->middleware(['permission:edit layanan']);
         Route::patch('layanan/{layanan}', 'LayananController@update')->middleware(['permission:edit layanan']);
         Route::delete('layanan/{layanan}', 'LayananController@destroy')->middleware(['permission:delete layanan']);
-        
+
+        // https://laravel.com/docs/7.x/controllers
+        // Route::apiResource('surat-keluar', 'SuratKeluarController');        
+        Route::get('surat-keluar', 'SuratKeluarController@index')->middleware(['permission:view surat keluar']);
+        Route::get('surat-keluar/{suratKeluar}', 'SuratKeluarController@show')->middleware(['permission:view surat keluar']);
+        Route::post('surat-keluar', 'SuratKeluarController@store')->middleware(['permission:add surat keluar']);
+        Route::patch('surat-keluar/{suratKeluar}', 'SuratKeluarController@update')->middleware(['permission:edit surat keluar']);
+        Route::put('surat-keluar/{suratKeluar}', 'SuratKeluarController@update')->middleware(['permission:edit surat keluar']);
+        Route::delete('surat-keluar/{suratKeluar}', 'SuratKeluarController@destroy')->middleware(['permission:delete surat keluar']);
+
+        Route::post('surat-keluar/{suratKeluar}/berkas', 'SuratKeluarController@tambahBerkas')->middleware(['permission:edit surat keluar']);
+        Route::delete('surat-keluar/{suratKeluar}/berkas', 'SuratKeluarController@hapusBerkas')->middleware(['permission:delete surat keluar']);
     });
 });
 
