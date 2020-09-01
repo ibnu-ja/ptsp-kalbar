19e15272c493506f104f71ca6dbf3e2cb9bd2576
ibnu-ja <ibnu.magang@gmail.com>
2020-09-01 10:26:11 +0700

    return nomor urut

--- app/Http/Resources/SuratKeluarResource.php
+++ app/Http/Resources/SuratKeluarResource.php
@@ -14,15 +14,20 @@ class SuratKeluarResource extends JsonResource
      */
     public function toArray($request)
     {
-        $berkas = $this->getMedia()->makeHidden(['created_at','updated_at', 'model_type', 'model_id', 'id']);
+        $berkas = $this->getMedia('berkas')->makeHidden(['created_at','updated_at', 'model_type', 'model_id', 'id']);
+        if ($this->jenis == 0) $nomor_urut = $this->nomor_urut_biasa;
+        if ($this->jenis == 1) $nomor_urut = $this->nomor_urut_spd;
+        if ($this->jenis == 2) $nomor_urut = $this->nomor_urut_sk;
         return [
             'id' => $this->id,
-            'nomor_urut' => $this->nomor_urut,
+            'nomor_urut' => $nomor_urut,
             'jenis' => $this->jenis,
             'tgl_surat' => $this->tgl_surat,
             'asal' => $this->asal,
-            'kode_asal' => $this->kode_asal,
+            'isi' => $this->isi,
+            'kode_satker' => $this->kode_satker,
             'berkas' => $this->when(!$berkas->isEmpty(), $berkas),
+            // 'berkas' => $this->when(!$berkas->isEmpty(), $berkas),
             // 'permohonan_count' => PermohonanResource::collection($this->whenLoaded('permohonan')->count()),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
